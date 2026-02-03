3738be58de3dfcc3e06417326f7ceb0bb4d45539
DevJorgeAlvino <dev.jorgealvino@gmail.com>
2026-02-03 03:02:42 -0500

    feat: make team_foreign_key column nullable in permission tables migration

--- database/migrations/2026_01_31_080631_create_permission_tables.php
+++ database/migrations/2026_01_31_080631_create_permission_tables.php
@@ -84,7 +84,7 @@ return new class extends Migration
                 ->on($tableNames['roles'])
                 ->onDelete('cascade');
             if ($teams) {
-                $table->ulid($columnNames['team_foreign_key']);
+                $table->ulid($columnNames['team_foreign_key'])->nullable();
                 $table->index($columnNames['team_foreign_key'], 'model_has_roles_team_foreign_key_index');
 
                 // $table->primary([$columnNames['team_foreign_key'], $pivotRole, $columnNames['model_morph_key'], 'model_type'],
