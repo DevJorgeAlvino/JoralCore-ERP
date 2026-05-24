ba2ddb4325356344dbf45cb9be136c1a03c653f7
DevJorgeAlvino <dev.jorgealvino@gmail.com>
2026-05-24 00:49:08 -0500

    fix(i18n): agregar archivos de traducción faltantes para el módulo de unidades de medida (unit_measures)

---
+++ resources/lang/en/unit_measures.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'title' => 'Unit Measures',
+    'single' => 'Unit Measure',
+
+    'sections' => [
+        'general' => 'General Information',
+        'general_desc' => 'Main details of the unit measure.',
+    ],
+
+    'fields' => [
+        'code' => 'Code',
+        'name' => 'Name',
+        'country' => 'Country',
+        'created_at' => 'Created At',
+        'updated_at' => 'Updated At',
+    ],
+];
